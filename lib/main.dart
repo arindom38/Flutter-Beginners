02d73297605c28f80005ed7e33870adafc21d3dd
Arindom Kundu <arindomkundu18@gmail.com>
2021-05-07 12:22:46 +0600

    Lesson 7 , Stateless widget and hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,37 @@ import 'package:flutter/material.dart';
 //MaterialApp enable google design
 void main() => runApp(MaterialApp(
   //Home page using scaffold widget like a wrapper
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Green Earth !!'),
-      centerTitle: true,
-      backgroundColor: Colors.green[600],
-    ),
-    body: Center(
-      child: Text(
-        'Plant Tress , Save Earth!',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[300],
-          fontFamily: 'FrederickatheGreat',
+  home: Home(),
+));
+
+//stateless widget is used for hot reload (without Building app fully)
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) { //This build Function build the app automatically and specific screen if changes
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Green Earth !!'),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: Center(
+        child: Text(
+          'Plant Tress\nSave Earth!',
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[300],
+            fontFamily: 'FrederickatheGreat',
+          ),
         ),
       ),
-    ),
-    backgroundColor: Colors.lightGreen[400],
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {}, // button key function
-      child: Text('Go'),
-      backgroundColor: Colors.green[200],
-    ),
-  ),
-));
+      backgroundColor: Colors.lightGreen[400],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {}, // button key function
+        child: Text('Go'),
+        backgroundColor: Colors.green[200],
+      ),
+    );
+  }
+}
